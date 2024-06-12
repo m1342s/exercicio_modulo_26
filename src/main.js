119d7ec9bfd6e2855ef24d2cfb884a14626f626e
Matheus Viersa <matheusviersa995@hotmail.com>
2024-06-12 10:56:37 -0300

    Entrega finalizada

---
+++ src/main.js
@@ -0,0 +1,8 @@
+"use strict";
+function multiplica(number1, number2) {
+    return number1 * number2;
+}
+function dizOla(nome) {
+    return "Olá" + " " + nome;
+}
+console.log(dizOla("matheus"));
---
+++ src/main.ts
@@ -0,0 +1,8 @@
+function multiplica(number1:number,number2:number):number {
+    return number1*number2
+}
+
+function dizOla(nome:string) {
+    return "Olá"+ " "+ nome
+}
+console.log(dizOla("matheus"))
